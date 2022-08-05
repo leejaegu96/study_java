55dc7c21c983b1626e1b33115909621dfc557fc4
leejaegu96 <worncjfrn@gmail.com>
2022-08-05 17:00:33 +0900

    test_0805

---
+++ src/com/web/EWord/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,29 @@
+package com.web.EWord.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+
+//		+=
+		int result = 10;
+		int result2 = 10;
+		int result3 = 10;
+		
+		result += 10;
+//		result = result + 10;
+		
+		System.out.println("result: " + result);
+
+//		-=
+		result2 -= 10;
+		
+		System.out.println("result2: " + result2);
+		
+//		*=
+		result3 *= 10;	
+		
+		System.out.println("result3: " + result3);
+		
+	}
+
+}
