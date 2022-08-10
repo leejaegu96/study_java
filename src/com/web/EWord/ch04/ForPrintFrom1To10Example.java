d2c1fcf5258d24318fbbae107c5c050627e574ee
leejaegu96 <worncjfrn@gmail.com>
2022-08-10 17:41:18 +0900

    test-0810

---
+++ src/com/web/EWord/ch04/ForPrintFrom1To10Example.java
@@ -0,0 +1,16 @@
+package com.web.EWord.ch04;
+
+public class ForPrintFrom1To10Example {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		
+		for(int i = 1; i <= 10; i++) {
+			sum = sum + i; // sum +=i;
+		}
+		System.out.println(sum);
+		
+	}
+
+}
---
+++ src/com/web/EWord/ch04/RandomSum.java
@@ -0,0 +1,55 @@
+package com.web.EWord.ch04;
+
+public class RandomSum {
+
+	public static void main(String[] args) {
+
+//		double num1 = 1;
+//		double num2 = 100;
+//		int sum = 0, min = 0, max = 0;
+//		
+//		int random1 = (int) ((Math.random() * (num2 - num1)) + num1);
+//		int random2 = (int) ((Math.random() * (num2 - num1)) + num1);
+//		
+//		if (random1 < random2) {
+//			max = random2;
+//			min = random1;
+//		} else if (random1 > random2) {
+//			max = random1;
+//			min = random2;
+//		} else {
+//			System.out.println("서로 다른 값을 입력하세요");
+//			return ;
+//		}
+//		System.out.println("min : " + min + ", " + "max : " + max);
+//		
+//		for (int i = min; i <= max; i++) {
+//			sum = sum + i;
+//			System.out.println(i + " : " + sum);
+//		}
+//		System.out.println("total : " + sum);
+		
+		int min = 1;
+		int max = 100;
+		int sum = 0;
+		
+		int random1 = (int) ((Math.random() * (max-min)) + min);
+		int random2 = (int) ((Math.random() * (max-min)) + min);
+		System.out.println("random1 : " + random1 + ", random2 : " + random2);
+		
+		if(random1 > random2) {
+			for(int i = random2; i <= random1; i++) {
+				sum = i + sum;
+			} System.out.println("두 수 사이의 합은 : " + sum);
+		}else if(random1 < random2) {
+			for(int i = random1; i <=random2; i++) {
+				sum = i + sum;
+			} System.out.println("두 수 사이의 합은 : " + sum);
+		}else {
+			sum = random1 + random2;
+			System.out.println("두 수 사이의 합은 : " + sum);
+		} 
+		
+	}
+
+}
