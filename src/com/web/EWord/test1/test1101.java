d92c9321cda4264957f8554ad6b50b1a20596e2d
leejaegu96 <worncjfrn@gmail.com>
2022-11-01 22:00:42 +0900

    test

---
+++ src/com/web/EWord/test1/test1101.java
@@ -0,0 +1,29 @@
+package com.web.EWord.test1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test1101 {
+	
+	public ArrayList<Integer> solution(int n, int[] arr){
+		ArrayList<Integer> answer = new ArrayList<>();
+		answer.add(arr[0]);
+		for(int i=1; i<n; i++){
+			if(arr[i]>arr[i-1]) answer.add(arr[i]);
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args){
+		test1101 T = new test1101();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++){
+			arr[i]=kb.nextInt();
+		}
+		for(int x : T.solution(n, arr)){
+			System.out.print(x+" ");
+		}
+	}
+}
